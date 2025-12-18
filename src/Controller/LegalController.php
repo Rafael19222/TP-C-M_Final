67385f39ebaad545bc7d52f84f5861b39aa50158
Rafael <spinola-rafael@dartybox.com>
2025-12-18 11:59:19 +0100

    Ajouter toutes les pages légales mentions, CGU, CGV, confidentialité, qui sommes-nous/Afficher les produits sur la page d'accueil

--- src/Controller/LegalController.php
+++ src/Controller/LegalController.php
@@ -6,13 +6,36 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/legal')]
 final class LegalController extends AbstractController
 {
-    #[Route('/legal', name: 'app_legal')]
-    public function index(): Response
+    #[Route('/mentions', name: 'app_legal_mentions')]
+    public function mentions(): Response
     {
-        return $this->render('legal/index.html.twig', [
-            'controller_name' => 'LegalController',
-        ]);
+        return $this->render('legal/mentions.html.twig');
+    }
+
+    #[Route('/cgu', name: 'app_legal_cgu')]
+    public function cgu(): Response
+    {
+        return $this->render('legal/cgu.html.twig');
+    }
+
+    #[Route('/cgv', name: 'app_legal_cgv')]
+    public function cgv(): Response
+    {
+        return $this->render('legal/cgv.html.twig');
+    }
+
+    #[Route('/confidentialite', name: 'app_legal_privacy')]
+    public function privacy(): Response
+    {
+        return $this->render('legal/privacy.html.twig');
+    }
+
+    #[Route('/qui-sommes-nous', name: 'app_legal_about')]
+    public function about(): Response
+    {
+        return $this->render('legal/about.html.twig');
     }
 }
